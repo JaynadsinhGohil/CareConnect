4cc11081d14a3e186395842eba13eb77cf0eaf1f
Jaynadsinh Gohil <gohiljaynadsinh1506@gmail.com>
2026-04-26 21:10:58 +0530

    Unecessary Files

--- backend/dist/config/database.d.ts
+++
@@ -1,4 +0,0 @@
-import pg from 'pg';
-declare const pool: pg.Pool;
-export default pool;
-//# sourceMappingURL=database.d.ts.map
--- backend/dist/models/schema.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const initializeDatabase: () => Promise<void>;
-export declare const seedDatabase: () => Promise<void>;
-//# sourceMappingURL=schema.d.ts.map
